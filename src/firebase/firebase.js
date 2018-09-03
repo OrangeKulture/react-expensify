8b4caff607a4b90d542a190ae573e62ad3195e30
OrangeKulture <JorgeA@corp.otscr.com>
2018-09-03 15:18:10 -0600

    firebase file updated

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,27 +1,27 @@
-import * as firebase from 'firebase';
+// import * as firebase from 'firebase';
 
-const config = {
-  apiKey: "AIzaSyCsHW2REmIBU8QS3EGosDJdgpHbwjuc6s4",
-  authDomain: "demoexpense-93f08.firebaseapp.com",
-  databaseURL: "https://demoexpense-93f08.firebaseio.com",
-  projectId: "demoexpense-93f08",
-  storageBucket: "demoexpense-93f08.appspot.com",
-  messagingSenderId: "1061886358392"
-};
+// const config = {
+//   apiKey: "AIzaSyCsHW2REmIBU8QS3EGosDJdgpHbwjuc6s4",
+//   authDomain: "demoexpense-93f08.firebaseapp.com",
+//   databaseURL: "https://demoexpense-93f08.firebaseio.com",
+//   projectId: "demoexpense-93f08",
+//   storageBucket: "demoexpense-93f08.appspot.com",
+//   messagingSenderId: "1061886358392"
+// };
 
-firebase.initializeApp(config);
-const database = firebase.database();
+// firebase.initializeApp(config);
+// const database = firebase.database();
 
-database.ref('expenses').on('value', (snapshot) => {
-  const expenses = [];
-  snapshot.forEach((child) => {
-    expenses.push({
-      id: child.key,
-      ...child.val()
-    })
-  })
-  console.log(expenses);
-})
+// database.ref('expenses').on('value', (snapshot) => {
+//   const expenses = [];
+//   snapshot.forEach((child) => {
+//     expenses.push({
+//       id: child.key,
+//       ...child.val()
+//     })
+//   })
+//   console.log(expenses);
+// })
 
 // const expenses = [
 //   {
