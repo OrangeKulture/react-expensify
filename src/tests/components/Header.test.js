ee53bda938927fbdfdc99729ec8b673ced7d6783
OrangeKulture <JorgeA@corp.otscr.com>
2018-09-11 10:23:10 -0600

    Login logic started

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import Header from '../../components/Header';
-
-// test('should render Header component', () => {
-//   const renderer = new ReactShallowRenderer();
-//   renderer.render(<Header />);
-//   expect(renderer.getRenderOutput()).toMatchSnapshot();
-// })
-
+import {Header} from '../../components/Header';
 
 test('should render Header component', () => {
-  const wrapper = shallow(<Header />);
-  // expect(wrapper.find('h1').text()).toBe('Expensify');
+  const wrapper = shallow(<Header startLogout={() => {}}/>);
   expect(wrapper).toMatchSnapshot();
 })
+
+test('should call startLogout on button click', () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout}/>);
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
+});
