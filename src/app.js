26e16165d93c9ccc91f1f8f4226032308db911db
OrangeKulture <JorgeA@corp.otscr.com>
2018-09-12 12:01:26 -0600

    added private routes, auth reducer and test

--- src/app.js
+++ src/app.js
@@ -4,13 +4,11 @@ import {Provider} from 'react-redux';
 import AppRouter, {history} from './routes/AppRouter';
 import configureStore from './store/configureStore';
 import {startSetExpenses} from './actions/expenses';
-import {setTextFilter} from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+import {login, logout} from './actions/auth';
+import {firebase} from './firebase/firebase';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import {firebase} from './firebase/firebase';
-import './playground/promises';
 
 
 const store = configureStore();
@@ -34,6 +32,7 @@ ReactDOM.render(<p>Loading..</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if(user){
+    store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
       if(history.location.pathname === '/') {
@@ -41,6 +40,7 @@ firebase.auth().onAuthStateChanged((user) => {
       }
     });
   } else {
+    store.dispatch(logout());
     renderApp();
     history.push('/');
   }
--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,3 +1,12 @@
-export default (state = {}) => {
-  
-}
+export default (state = {},action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        uid: action.uid
+      };
+    case 'LOGOUT':
+      return {};
+    default:
+      return state;
+  };
+};
