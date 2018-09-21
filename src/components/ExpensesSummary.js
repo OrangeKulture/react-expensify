18a5b0a8b16294a5489b817722856b0767ee0b78
OrangeKulture <JorgeA@corp.otscr.com>
2018-09-20 18:08:46 -0600

    styling dashboard page

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import numeral from 'numeral';
+import {Link} from 'react-router-dom';
 import getVisibleExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 
 export const ExpensesSummary = ({expenseCount, expensesTotal}) => {
   const word = expenseCount === 1 ? 'expense' : 'expenses';
   return (
-    <div>
-      <h1>Viewing {expenseCount} {word} totaling {numeral(expensesTotal / 100).format('$0,0.00')}</h1>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {word} totaling <span>{numeral(expensesTotal / 100).format('$0,0.00')}</span></h1>
+        <div className="page-header__actions">
+          <Link className="button-main" to="/create">Add Expense</Link>
+        </div>
+      </div>
     </div>
   )
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export const Header = ({startLogout}) => (
     <div className="content-container">
       <div className="header__content">
         <Link className="header__title" to="/dashboard"><h1>Expensify</h1></Link>
-        <button onClick={startLogout}>Logout</button>
+        <button onClick={startLogout} className="button-main button-main--link">Logout</button>
       </div>
     </div>
   </header>
